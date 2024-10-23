3445abf959e58ee29390839900286c06e493db2a
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2024-10-23 20:46:01 +0200

    Try Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.11.2"
@@ -12,23 +11,21 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.28")
-    implementation("org.projectlombok:lombok:1.18.28")
-    implementation("org.reflections:reflections:0.9.11")
-    implementation("org.knowm.xchart:xchart:3.8.7")
+    // Lombok
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
     testCompileOnly("org.projectlombok:lombok:1.18.30")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 
-    // JUnit 5 (JUnit Jupiter API)
+    // Reflections and Charting
+    implementation("org.reflections:reflections:0.9.11")
+    implementation("org.knowm.xchart:xchart:3.8.7")
+
+    // Testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    // JUnit Jupiter Engine for running tests
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-    // Mockito for mocking objects
     testImplementation("org.mockito:mockito-core:4.11.0")
     testImplementation("org.mockito:mockito-inline:4.0.0")
-    // Mockito extension for JUnit 5 integration
     testImplementation("org.mockito:mockito-junit-jupiter:4.11.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.powermock:powermock-module-junit4:2.0.9")
@@ -36,12 +33,11 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+// Configure IntelliJ Plugin
 intellij {
     version.set("2022.2.5")
     type.set("IC") // Target IDE Platform
@@ -49,13 +45,13 @@ intellij {
 }
 
 tasks {
-    // Set the JVM compatibility versions
+    // Ensure compatibility with Java 17
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
 
-    test{
+    test {
         useJUnitPlatform()
     }
 
